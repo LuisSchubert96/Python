78f7751a8386916b3d4a4508d6bc7fd9890405eb
Luís Schubert <luishvianna@outlook.com>
2025-03-04 15:58:45 -0300

    variable.py

---
+++ Variable/variable.py
@@ -0,0 +1,11 @@
+print ("Hello! What is your name?")
+name = "Luís"
+print (f"Nice to meet you,{name}")
+print ("How old are you?")
+age = 28
+bot_age = 3
+age_difference = age - bot_age
+print (f"You are {age_difference} years older than me. I'm only {bot_age} years old")
+print ("What's your favorite color?")
+color = "Blue"
+print (f"Oh {color} is a beautiful color!")
