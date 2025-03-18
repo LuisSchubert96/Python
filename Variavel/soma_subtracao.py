923e5846c563900de5697e8d09b3bed2e116d496
Luís Schubert <luishvianna@outlook.com>
2025-03-18 15:08:24 -0300

    Add files via upload

---
+++ Variavel/soma_subtracao.py
@@ -0,0 +1,7 @@
+num1 = 10
+num2 = 20
+num3 = 10
+soma = num1 + num2 + num3
+subtracao = num1 - num2 - num3
+print (f'A soma dos valores é de:{soma}')
+print (f'A subtração dos valores é de:{subtracao}')
